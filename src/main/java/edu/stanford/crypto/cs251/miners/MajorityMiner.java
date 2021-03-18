e649c0c864784a7b5e206a93aae74a0081b85f52
Ford neild <hanfordn35@gmail.com>
2021-03-18 16:53:23 -0400

    majority miner barely underpreforming

---
+++ src/main/java/edu/stanford/crypto/cs251/miners/MajorityMiner.java
@@ -0,0 +1,55 @@
+package edu.stanford.crypto.cs251.miners;
+
+import edu.stanford.crypto.cs251.blockchain.Block;
+import edu.stanford.crypto.cs251.blockchain.NetworkStatistics;
+
+public class MajorityMiner extends BaseMiner implements Miner {
+    private Block currentHead;
+
+    public MajorityMiner(String id, int hashRate, int connectivity) {
+        super(id, hashRate, connectivity);
+
+    }
+
+    @Override
+    public Block currentlyMiningAt() {
+        return currentHead;
+    }
+
+    @Override
+    public Block currentHead() {
+        return currentHead;
+    }
+
+
+    @Override
+    public void blockMined(Block block, boolean isMinerMe) {
+        if(isMinerMe) {
+            if (block.getHeight() > currentHead.getHeight()) {
+                this.currentHead = block;
+            }
+        }
+        else{
+            if (currentHead == null) {
+                //out chain is empty
+                currentHead = block;
+            } else if (block != null && block.getHeight() > currentHead.getHeight()+7) {
+                // they are ahead of us by alot, lets give up and join thier chain
+                this.currentHead = block;
+            }else{
+
+            }
+        }
+    }
+
+
+    @Override
+    public void initialize(Block genesis, NetworkStatistics networkStatistics) {
+        this.currentHead = genesis;
+    }
+
+    @Override
+    public void networkUpdate(NetworkStatistics statistics) {
+
+    }
+}
--- src/test/java/edu/stanford/crypto/cs251/miners/MiningSimulation.java
+++ src/test/java/edu/stanford/crypto/cs251/miners/MiningSimulation.java
@@ -49,7 +49,7 @@ public class MiningSimulation {
 
     @Test
     public void simulate51PercentAttack1() {
-        Miner attacker = new CompliantMiner("Attacker", 520, 1);
+        Miner attacker = new MajorityMiner("Attacker", 520, 1);
         Miner miner2 = new CompliantMiner("Miner2", 200, 1);
         Miner miner3 = new CompliantMiner("Miner3", 130, 1);
         Miner miner4 = new CompliantMiner("Miner4", 90, 1);
@@ -64,7 +64,7 @@ public class MiningSimulation {
         Assertions.assertThat(relativeProfits.get(attacker.getId())).isGreaterThan(.55);
     }
 
-/*
+
     @Test
     public void simulate51PercentAttack2() {
         Miner attacker = new MajorityMiner("Attacker", 550, 1);
@@ -80,7 +80,7 @@ public class MiningSimulation {
         Map<String, Double> relativeProfits = runSimulation(miners, BlockReward.ONE, churn);
         Assertions.assertThat(relativeProfits.get(attacker.getId())).isGreaterThan(.6);
     }
-
+/*
     @Test
     public void simulateSelfishMining1() {
         Miner attacker = new SelfishMiner("Attacker", 34, 1);
