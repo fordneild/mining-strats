b758afc724667d8d221ff6a39675b589818a95a6
Ford neild <hanfordn35@gmail.com>
2021-03-20 18:38:40 -0400

    scoring 60% one in every 3 or 4 runs

--- src/main/java/edu/stanford/crypto/cs251/miners/MajorityMiner.java
+++ src/main/java/edu/stanford/crypto/cs251/miners/MajorityMiner.java
@@ -5,6 +5,7 @@ import edu.stanford.crypto.cs251.blockchain.NetworkStatistics;
 
 public class MajorityMiner extends BaseMiner implements Miner {
     private Block currentHead;
+    private double hashPercent;
 
     public MajorityMiner(String id, int hashRate, int connectivity) {
         super(id, hashRate, connectivity);
@@ -22,23 +23,48 @@ public class MajorityMiner extends BaseMiner implements Miner {
     }
 
 
+
     @Override
     public void blockMined(Block block, boolean isMinerMe) {
         if(isMinerMe) {
             if (block.getHeight() > currentHead.getHeight()) {
                 this.currentHead = block;
             }
-        }
-        else{
-            if (currentHead == null) {
-                //out chain is empty
-                currentHead = block;
-            } else if (block != null && block.getHeight() > currentHead.getHeight()+7) {
-                // they are ahead of us by alot, lets give up and join thier chain
-                this.currentHead = block;
+        }else{
+            if(this.hashPercent > .5){
+                this.blockMinedMajority(block);
             }else{
-
+                this.blockMinedNormal(block);
             }
+        }
+    }
+
+    private void blockMinedNormal(Block block) {
+        if (currentHead == null) {
+            currentHead = block;
+        } else if (block != null && block.getHeight() > currentHead.getHeight()) {
+            this.currentHead = block;
+
+        }
+    }
+
+    private void blockMinedMajority(Block block) {
+        // 1 => very agressive
+        // 100 => not so agressive
+        int BASE_DISTANCE_WE_CAN_WIN_FROM = 8;
+        int AGRESSIVENESS_DAMPNER = 7;
+        int hashPercentAgressivenessBonus = (int) Math.round(((this.hashPercent*100) - 50)/AGRESSIVENESS_DAMPNER);
+        int DISTANCE_WE_CAN_WIN_FROM = BASE_DISTANCE_WE_CAN_WIN_FROM + hashPercentAgressivenessBonus;
+        if (currentHead == null) {
+            //out chain is empty
+            currentHead = block;
+        } else if (block != null && block.getHeight() > currentHead.getHeight()+DISTANCE_WE_CAN_WIN_FROM) {
+            // they are ahead of us by alot, lets give up and join thier chain
+            Block curBlock = block;
+            for(int i =0; i<DISTANCE_WE_CAN_WIN_FROM; i++){
+                curBlock = block.getPreviousBlock();
+            }
+            this.currentHead = curBlock;
         }
     }
 
@@ -46,10 +72,16 @@ public class MajorityMiner extends BaseMiner implements Miner {
     @Override
     public void initialize(Block genesis, NetworkStatistics networkStatistics) {
         this.currentHead = genesis;
+        updateIsMajority(networkStatistics);
+
     }
 
     @Override
     public void networkUpdate(NetworkStatistics statistics) {
+        updateIsMajority(statistics);
+    }
 
+    private void updateIsMajority(NetworkStatistics statistics) {
+        this.hashPercent = (this.getHashRate()*1.0)/ statistics.getTotalHashRate();
     }
 }
