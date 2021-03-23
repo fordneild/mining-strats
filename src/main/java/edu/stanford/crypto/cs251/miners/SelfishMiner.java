45e67fceb6b7f6c746e92d49ac606e927c619a3a
Ford neild <hanfordn35@gmail.com>
2021-03-23 16:12:55 -0400

    comments

--- src/main/java/edu/stanford/crypto/cs251/miners/SelfishMiner.java
+++ src/main/java/edu/stanford/crypto/cs251/miners/SelfishMiner.java
@@ -25,18 +25,26 @@ public class SelfishMiner extends BaseMiner {
     @Override
     public void blockMined(Block newBlock, boolean isMinerMe) {
         if(isMinerMe == true) {
+            //if i mined this block
             if (newBlock.getHeight() > this.privateHead.getHeight()) {
+                // if its a new block
+                // it now becomes what I am extending
                 this.privateHead = newBlock;
+                // but i dont publish it
             }
         }else{
-            if(newBlock != null && newBlock.getHeight() >= this.publicHead.getHeight()){
+            if(newBlock != null && newBlock.getHeight() > this.publicHead.getHeight()){
+                // if someone broadcasted a new block that is newer than my pubilic head
                 if(newBlock.getHeight() > this.privateHead.getHeight()){
+                    // if its also better than my private one, adpot it
                     this.privateHead = newBlock;
                     this.publicHead = newBlock;
                 }else{
                     int lead =  this.privateHead.getHeight() - newBlock.getHeight();
+                    // make the publicHead
                     this.publicHead = this.privateHead;
                     while(lead > 0 && this.publicHead.getPreviousBlock() != null){
+                        // announce a new head that is in front of their
                         this.publicHead = this.publicHead.getPreviousBlock();
                         lead--;
                     }
