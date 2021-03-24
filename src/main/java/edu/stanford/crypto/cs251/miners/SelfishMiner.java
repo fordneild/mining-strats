494b4e9294b0adbb513bae3074414316de673536
joaokawase <jkawase1@jhu.edu>
2021-03-23 20:42:28 -0400

    finished selfishMiner

--- src/main/java/edu/stanford/crypto/cs251/miners/SelfishMiner.java
+++ src/main/java/edu/stanford/crypto/cs251/miners/SelfishMiner.java
@@ -19,14 +19,15 @@ public class SelfishMiner extends BaseMiner {
 
     @Override
     public Block currentHead() {
-        double connectivity = (double) this.getConnectivity() / this.curNetStats.getTotalConnectivity();
+        //double connectivity = (double) this.getConnectivity() / this.curNetStats.getTotalConnectivity();
+        double hash_percent = (double) this.getHashRate() / this.curNetStats.getTotalHashRate();
         // int TO_ATTACK = 10;
         int TO_ATTACK = 5;
         int lead =  this.privateHead.getHeight() - this.publicHead.getHeight();
         if(lead > TO_ATTACK){
             // make the publicHead
             this.publicHead = this.privateHead;
-            int adv = connectivity > .30 ? 0 : 1;
+            int adv = hash_percent > .37 ? 0 : 1;
             // .30 is arbitrary but it seems to work well for maintaining consistency
             while(lead > adv && this.publicHead.getPreviousBlock() != null){
                 // announce a new head that is in front of theirs
@@ -40,9 +41,9 @@ public class SelfishMiner extends BaseMiner {
 
     @Override
     public void blockMined(Block newBlock, boolean isMinerMe) {
-        double connectivity = (double) this.getConnectivity() / this.curNetStats.getTotalConnectivity();
+        double hash_percent = (double) this.getHashRate() / this.curNetStats.getTotalHashRate();
         // int adv = connectivity > .30 ? 2 : 1;
-        int adv = connectivity > .30 ? 0 : 1;
+        int adv = hash_percent > .37 ? 0 : 1;
         if(isMinerMe == true) {
             //if i mined this block
             if (newBlock.getHeight() > this.privateHead.getHeight()) {
@@ -58,7 +59,7 @@ public class SelfishMiner extends BaseMiner {
                     // if its also better than my private one, adopt it
                     this.privateHead = newBlock;
                     this.publicHead = newBlock;
-                } else if (connectivity > .30 || newBlock.getHeight() + adv >= this.privateHead.getHeight()){
+                } else if (hash_percent > .37 || newBlock.getHeight() + adv >= this.privateHead.getHeight()){
                 // } else {
                     int lead =  this.privateHead.getHeight() - newBlock.getHeight();
                     // make the publicHead
